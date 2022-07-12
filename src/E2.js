d2f1b11f47a66e8c4ab82a8bdb3c6a3d793e4c8e
Roberto Alonso <robertinage@gmail.com>
2022-07-12 15:24:03 +0200

    Enunciado del E2 anhadido

--- src/E2.js
+++ src/E2.js
@@ -1,1 +1,11 @@
-console.log('Info JIAR');
+const elements = ["lorem", "ipsum", "dolor", "sit", "amet"];
+const index = 2;
+const newValue = "furor";
+
+const replaceAt = (arr, index, newElement) => {
+
+};
+
+const result = replaceAt(elements, index, newValue);
+console.log(result === elements); // false
+console.log(result); // ['lorem', 'ipsum', 'furor', 'sit', 'amet'];
