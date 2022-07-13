a34e0a82ad062d453efb600e8801d2e5b1089430
Roberto Alonso <robertinage@gmail.com>
2022-07-13 09:10:01 +0200

    Modified so it uses: spread operator & slice

--- src/E2.js
+++ src/E2.js
@@ -5,11 +5,13 @@ const newValue = "furor";
 const replaceAt = (arr, index, newElement) => {
     // Creamos copias para no usar los originales
     let arr_i = [...arr];
-    let index_i = index;
-    let newElement_i = newElement;
+    let arr_val = [...arr].slice(index, index+1);
 
-    // Sustituimos la posicion index_i del array arr_i con el valor newElement_i
-    arr_i[index_i] = newElement_i;
+    // Cambiamos el valor por el que queremos
+    arr_val = newElement;
+        
+    // Sustituimos la posicion index del array arr_i con el valor newElement_i
+    arr_i[index] = arr_val;
 
     return arr_i;
 };
