1fcdfbf353678c0d555afef37bfce01d87576533
Roberto Alonso <robertinage@gmail.com>
2022-07-13 08:39:47 +0200

    E2 funciona salvo result === elements

--- src/E2.js
+++ src/E2.js
@@ -3,10 +3,12 @@ const index = 2;
 const newValue = "furor";
 
 const replaceAt = (arr, index, newElement) => {
+    // Creamos copias para no usar los originales
     let arr_i = arr;
     let index_i = index;
     let newElement_i = newElement;
 
+    // Sustituimos la posicion index_i del array arr_i con el valor newElement_i
     arr_i[index_i] = newElement_i;
 
     return arr_i;
