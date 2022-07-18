4098a01387f02f02c4c471b352c9100a7b3604ee
Roberto Alonso <robertinage@gmail.com>
2022-07-18 08:56:00 +0200

    E1 to E5 created but not finished

---
+++ src/TypeScript/E1.ts
@@ -0,0 +1,19 @@
+// const students: unknown = [
+//     {
+//       name: "Patrick Berry",
+//       age: 25,
+//       occupation: "Medical scientist",
+//     },
+//     {
+//       name: "Alice Garner",
+//       age: 34,
+//       occupation: "Media planner",
+//     },
+//   ];
+  
+//   const logStudent = ({ name, age }: unknown) => {
+//     console.log(`  - ${name}, ${age}`);
+//   };
+  
+//   console.log("Students:");
+//   students.forEach(logStudent);
---
+++ src/TypeScript/E2.ts
@@ -0,0 +1,34 @@
+// interface Teacher {
+//     name: string;
+//     age: number;
+//     subject: string;
+//   }
+  
+//   const users: Teacher[] = [
+//     {
+//       name: "Luke Patterson",
+//       age: 32,
+//       occupation: "Internal auditor",
+//     },
+//     {
+//       name: "Jane Doe",
+//       age: 41,
+//       subject: "English",
+//     },
+//     {
+//       name: "Alexandra Morton",
+//       age: 35,
+//       occupation: "Conservation worker",
+//     },
+//     {
+//       name: "Bruce Willis",
+//       age: 39,
+//       subject: "Biology",
+//     },
+//   ];
+  
+//   const logUser = ({ name, age }: Teacher) => {
+//     console.log(`  - ${name}, ${age}`);
+//   };
+  
+//   users.forEach(logUser);
