171b2df0969c307cb49bc3f75a2334b4fcc7c231
Roberto Alonso <robertinage@gmail.com>
2022-07-19 09:33:55 +0200

    E1 Unexpected Identifier

--- src/TypeScript/E1.ts
+++ src/TypeScript/E1.ts
@@ -1,19 +1,25 @@
-// const students: unknown = [
-//     {
-//       name: "Patrick Berry",
-//       age: 25,
-//       occupation: "Medical scientist",
-//     },
-//     {
-//       name: "Alice Garner",
-//       age: 34,
-//       occupation: "Media planner",
-//     },
-//   ];
+interface Student {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+const Student: Student[] = [
+    {
+      name: "Patrick Berry",
+      age: 25,
+      occupation: "Medical scientist",
+    },
+    {
+      name: "Alice Garner",
+      age: 34,
+      occupation: "Media planner",
+    },
+  ];
   
-//   const logStudent = ({ name , age }: unknown) => {
-//     console.log(`  - ${name}, ${age}`);
-//   };
+  const logStudent = ({ name , age }: Student) => {
+    console.log(`  - ${name}, ${age}`);
+  };
   
-//   console.log("Students:");
-//   students.forEach(logStudent);
+  console.log("Student:");
+  Student.forEach(logStudent);
