e6f1122d24a77e023a3a9a6c59fd99790da7eb41
Roberto Alonso <robertinage@gmail.com>
2022-07-19 09:57:49 +0200

    E3 Done?

--- src/TypeScript/E3.ts
+++ src/TypeScript/E3.ts
@@ -1,9 +1,43 @@
-// const logUser = (user: User) => {
-//     let extraInfo: string;
-//     if (user.occupation) {
-//       extraInfo = user.occupation;
-//     } else {
-//       extraInfo = user.subject;
-//     }
-//     console.log(`  - ${user.name}, ${user.age}, ${extraInfo}`);
-//   };
+interface Teacher {
+    name: string;
+    age: number;
+    subject?: string;
+    occupation?: string;
+}
+
+const users: Teacher[] = [
+    {
+        name: "Luke Patterson",
+        age: 32,
+        occupation: "Internal auditor",
+    },
+    {
+        name: "Jane Doe",
+        age: 41,
+        subject: "English",
+    },
+    {
+        name: "Alexandra Morton",
+        age: 35,
+        occupation: "Conservation worker",
+    },
+    {
+        name: "Bruce Willis",
+        age: 39,
+        subject: "Biology",
+    },
+];
+
+const logUser = (user: Teacher) => {
+    let extraInfo: string = "";
+    
+    if (user.occupation) {
+        extraInfo = user.occupation;
+    } else if (user.subject) {
+        extraInfo = user.subject;
+    }
+
+    console.log(`  - ${user.name}, ${user.age}, ${extraInfo}`);
+};
+
+users.forEach(logUser);
